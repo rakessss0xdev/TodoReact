f54ef1eaafa929798c3deec59ec2c21932ca078a
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2024-12-17 21:17:20 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
-import './App.css'
+import React, {useState} from "react";
+import './App.css';
 
 function App() {
+  const [ task, settask ] = useState('')
+  const [ tasks, settasks ] = useState([]);
+  
+  const addTask = () => {
+    if(task.trim() !== ""){
+      settasks([...tasks, {id: Date.now(), text: task}])
+      settask('');
+    }
+  }
+
   return (
-    <div style={{height: '100vh', margin: '0'}}>
-      <h3>
-        Todo List                
-      </h3>
-      <TodoLogic/>
+    <div style={{height: "100vh", margin: "0", textAlign: "center"}}>
+      <h3>Todo App</h3>
+      <todologic task={task} tasks={settasks} addTask={addTask} />
+
+    <div>
+      {task.map((t) => {
+        <div
+          key={t.id}
+          style={{
+            margin: "10px",
+            borderRadius: "5px",
+            padding: "10px",
+            display: "inline-block"
+          }}
+        >
+          <span>
+            {t.text}
+          </span>
+        </div>
+      })}
     </div>
+  </div>
   )
 }
 
-function TodoLogic() {
+function todologic({addTask, todologic, task }) {
   return (
-    <div id='container' style={{ height: "500px", margin: "10px", backgroundColor: "white", width: "500px" }}>
-      <input type={'text'} 
-      placeholder={"Add Your Todos"}
-      style={{padding: "10px", margin: "20px", borderRadius: "10px"}}/>
-    </div>       
-  );
+    <div
+      id="conatiner"
+      style={{
+        height: "150px",
+        margin: "10px auto",
+        backgroundColor: "white",
+        width: "500px",
+        borderRadius: "5px"
+      }}
+    >
+      <input 
+        type="text"
+        placeholder="Add your Todos"
+        value={task}
+        
+    </div>
+  )
 }
-
-
-
-export default App
